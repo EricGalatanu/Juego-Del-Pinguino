7a76dca37d9590974f42fb72b84f30ccff19111e
praatts <127567514+praatts@users.noreply.github.com>
2025-03-28 15:34:06 +0100

    Creación constructor dado

--- src/controlador/Dado.java
+++ src/controlador/Dado.java
@@ -1,5 +1,16 @@
 package controlador;
-
+import java.util.Random;
+import java.util.Scanner;
 public class Dado {
 
+	private String tipo;
+	private int valor; 
+	Random r = new Random();
+	Scanner s = new Scanner(System.in);
+	 public Dado (String tipo, int valor) {
+		this.tipo = tipo;
+		this.valor = valor;
+	}
+	
+	
 }
