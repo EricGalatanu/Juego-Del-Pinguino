db972587ffadb7540403dd7dc6a6caa2969b38dc
333ares <aresgp5335@gmail.com>
2025-03-28 16:12:52 +0100

    Creación de los atributos, constructor y getters y setters
    Creamos dentro de la clase vista sus respectivos atributos, su constructor y los getters y setters correspondientes.

--- src/vista/TipoJugador.java
+++ src/vista/TipoJugador.java
@@ -2,4 +2,28 @@ package vista;
 
 public class TipoJugador {
 
+	private String humano;
+	private String cpu;
+	
+	public TipoJugador (String humano, String cpu) {
+		this.humano = humano;
+		this.cpu = cpu;
+	}
+	
+	public String getHumano() {
+		return humano;
+	}
+	
+	public void setHumano(String humano) {
+		this.humano = humano;
+	}
+	
+	
+	public String getCpu() {
+		return cpu;
+	}
+	
+	public void setCpu(String cpu) {
+		this.cpu = cpu;
+	}	
 }
