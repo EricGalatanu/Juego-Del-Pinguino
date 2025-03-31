8d3104a3660e77e79a92f54de0437b352619f2f1
praatts <127567514+praatts@users.noreply.github.com>
2025-03-31 19:56:24 +0200

    Creación constructor, getters y setters Clase Inventario

--- src/controlador/Inventario.java
+++ src/controlador/Inventario.java
@@ -1,5 +1,47 @@
 package controlador;
-
+import java.util.*;
 public class Inventario {
 
+	private ArrayList<Dado> dados;
+	private int peces;
+	private int bolasDeNieve;
+	
+	public Inventario(ArrayList<Dado> dados, int peces, int bolasDeNieve) {
+		this.dados = dados;
+		this.peces = peces;
+		this.bolasDeNieve = bolasDeNieve;
+	}
+
+	public ArrayList<Dado> getDados() {
+		return dados;
+	}
+
+	public void setDados(ArrayList<Dado> dados) {
+		this.dados = dados;
+	}
+
+	public int getPeces() {
+		return peces;
+	}
+
+	public void setPeces(int peces) {
+		this.peces = peces;
+	}
+
+	public int getBolasDeNieve() {
+		return bolasDeNieve;
+	}
+
+	public void setBolasDeNieve(int bolasDeNieve) {
+		this.bolasDeNieve = bolasDeNieve;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
