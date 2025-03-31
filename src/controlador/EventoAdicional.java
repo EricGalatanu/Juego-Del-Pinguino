e307a794dfbdbc5f99096980fa0447a76b529dd9
praatts <127567514+praatts@users.noreply.github.com>
2025-03-31 19:52:12 +0200

    Creación constructor Evento Adicional

--- src/controlador/EventoAdicional.java
+++ src/controlador/EventoAdicional.java
@@ -1,5 +1,13 @@
 package controlador;
 
 public class EventoAdicional extends Evento {
+	
+	public EventoAdicional(int idEvento, String infoEvento, int idEventoAdicional) {
+		super(idEvento, infoEvento);
+	}
 
+
+	
+	
+	
 }
