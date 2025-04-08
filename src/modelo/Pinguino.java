892cfab20d24942e459345078a55ea3718109053
praatts <127567514+praatts@users.noreply.github.com>
2025-04-08 16:16:26 +0200

    Método creación pinguino + getter/setters identificador
    creación del método que pregunta al usuario su nombre y color del pingüino y crea el objeto pingüino correspondiente

--- src/modelo/Pinguino.java
+++ src/modelo/Pinguino.java
@@ -1,16 +1,23 @@
 package modelo;
 
+import java.util.Scanner;
+
+import testRepte.Inventario;
+import testRepte.Pinguino;
+
 public class Pinguino {
 
 private String nombre;
 	public String color;
 	public int posicion;
+	private int id;
 	public Inventario inv;
 	public String tipo;
 	
-	public Pinguino (String nombre, String color, int posicion, Inventario inv, String tipo) {
+	public Pinguino (String nombre, String color, int id, int posicion, Inventario inv, String tipo) {
 		this.nombre = nombre;
 		this.color = color;
+		this.id = id;
 		this.posicion = 0;
 		this.inv = inv;
 		this.tipo = tipo;
@@ -40,13 +47,6 @@ private String nombre;
 		this.posicion = posicion;
 	}
 	
-	public String getInventario() {
-		return inv;
-	}
-	
-	public void setInventario(Inventario inv) {
-		this.inv = inv;
-	}
 	
 	public String getTipo() {
 		return tipo;
@@ -56,6 +56,49 @@ private String nombre;
 		this.tipo = tipo;
 	}
 	
+	public Pinguino crearPinguino(Pinguino pingu) {
+		Scanner s = new Scanner(System.in);
+		int identificador = 1;
+		String nombre = "";
+		String color = "";
+		int posicion = 0;
+		Inventario inv = new Inventario(0, 0, 0);
+		String tipo = "Jugador";
+		System.out.println("Introduzca su nombre");
+		nombre = s.nextLine();
+		System.out.println("Introduzca el color de su pingüino");
+		color = s.nextLine();
+
+		pingu.setNombre(nombre);
+		pingu.setColor(color);
+		pingu.setId(identificador);
+		pingu.setPosicion(posicion);
+		pingu.setInventario(inv);
+		pingu.setTipo(tipo);
+
+		System.out.println("Se ha creado su pingüino correctamente!");
+		
+		return pingu;
+	}
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Inventario getInv() {
+		return inv;
+	}
+
+	public void setInv(Inventario inv) {
+		this.inv = inv;
+	}
+
 	public void avanzar() {
 		this.posicion++;
 	}
